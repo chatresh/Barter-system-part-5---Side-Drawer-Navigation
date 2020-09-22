06126dd08862198ebe036804c9bd14f898a2822b
chatresh <60438260+chatresh@users.noreply.github.com>
2020-09-22 11:27:32 +0530

    Add files via upload

---
+++ components/settings.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View,Image,TouchableOpacity} from 'react-native';
+
+
+export default class CustomSideBarMenu extends React.Component{
+    render(){
+        return(
+          <Text style={{marginTop:270,marginLeft:550,fontSize:22}}>..........This Screen is Under Maintanance...</Text>
+        )
+    }
+}
